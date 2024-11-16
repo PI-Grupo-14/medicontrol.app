be8ea623243f3bb71fa2134131cf9228a09ce094
fabiosoares-silva <fabiosores.soares@gmail.com>
2024-11-16 13:18:30 -0300

    Deixar apenas o projeto frontend

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import createApp from './app';
-
-const app = createApp();
-const port = process.env.PORT;
-
-app.listen(port, () => {
-    console.log(`Tudo certo!😎\nA aplicação MediControl  está rodando na porta http://localhost:${port}⚡`);
-});
